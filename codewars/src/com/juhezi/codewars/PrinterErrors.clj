41fd044f76830ad29b5cd31065a26a5444e55273
Juhezi <juhezix@163.com>
2017-05-15 17:51:13 +0800

    update commit

--- codewars/src/com/juhezi/codewars/PrinterErrors.clj
+++ codewars/src/com/juhezi/codewars/PrinterErrors.clj
@@ -3,7 +3,7 @@
 ; URL: https://www.codewars.com/kata/printer-errors/clojure
 
 (defn printer-error [s]
-  (str (count (re-seq #"[n-z]" s)) "/" (count s))           ;使用正则表达式进行匹配
+  (str (count (re-seq #"[n-z]" s)) "/" (count s))           ;使用正则表达式进行匹配，Clojure 里面把以 # 开头的字符串当作正则表达式
   )
 
 (println (printer-error "aaaxbbbbyyhwawiwjjjwwm"))

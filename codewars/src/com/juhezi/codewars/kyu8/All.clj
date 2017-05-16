a9be9b792ec339b0d73947b4a3659433de7b8072
Juhezi <juhezix@163.com>
2017-05-16 09:16:56 +0800

    Parse nice int from char problem

--- codewars/src/com/juhezi/codewars/kyu8/All.clj
+++ codewars/src/com/juhezi/codewars/kyu8/All.clj
@@ -5,3 +5,12 @@
 (defn repeat-str [n s]
   (apply str (repeat n s)))
 
+;Parse nice int from char problem
+;URL: https://www.codewars.com/kata/parse-nice-int-from-char-problem/train/clojure
+(defn how-old
+  [her-old]
+  (- (int (nth her-old 0)) 48)
+  )
+
+;clever
+(def how-old read-string)                                   ;read-String 可以运行代码，如果以数字开头，则默认读取数字，知道遇见空格，否则，则作为字符串读取

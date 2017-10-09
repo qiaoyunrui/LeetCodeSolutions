e4b2a6466396fbee0254d60c59f34c2f98966f65
Juhezi <juhezix@163.com>
2017-10-09 21:40:28 +0800

    [Update]

---
+++ codewars/src/com/juhezi/codewars/kyu5/MaxSubarraySum.py
@@ -0,0 +1,13 @@
+def maxSequence(arr):
+    sum = 0
+    maxSum = 0
+    for i in arr:
+        sum += i
+        if sum < 0:
+            sum = 0
+        else:
+            if sum > maxSum:
+                maxSum = sum
+    return maxSum
+
+print(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
---
+++ codewars/src/com/juhezi/codewars/kyu5/MaximumSubarraySum.clj
@@ -0,0 +1,7 @@
+(ns com.juhezi.codewars.kyu5.MaximumSubarraySum)
+
+; URL: https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/clojure
+; Maximum subarray sum
+(defn max-sequence [xs]
+  ;todo
+  )
